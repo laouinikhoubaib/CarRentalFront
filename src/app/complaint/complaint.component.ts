8bd742a1c48bd31305d5fd96554c47243e6d8669
laouinikhoubaib <73175459+laouinikhoubaib@users.noreply.github.com>
2023-04-13 07:47:28 +0100

    gestion des réclamations(les filtres ,dashboard)

--- src/app/complaint/complaint.component.ts
+++ src/app/complaint/complaint.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Complaint } from '../models/complaint';
 import { ComplaintService } from '../shared/complaint.service';
-import {AuthenticationService} from '../shared/authentication.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ConfirmationService, MessageService} from 'primeng/api';
-import {Agence} from '../models/agence';
 import {Subscription} from 'rxjs/dist/types';
-
+import * as Chart from 'chart.js';
+import * as Highcharts from 'highcharts';
 
 @Component({
     selector: 'app-complaint',
@@ -33,13 +32,19 @@ import {Subscription} from 'rxjs/dist/types';
 })
 export class ComplaintComponent implements OnInit {
 
+    @ViewChild('pieChart') pieChart: ElementRef;
+    @ViewChild('pieChartt') pieChartt: ElementRef;
+    @ViewChild('complaintsChart') private complaintsChartRef!: ElementRef;
 
+
+    private complaintsChart: Chart | null = null;
+    chart: any;
     complaints: Complaint[];
     userId: number;
     complaint: Complaint;
     private routeSub: Subscription;
-    complaintsByType: string[] = [];
     selectedType: string;
+
 
     constructor(private router: Router, private messageService: MessageService, private complaintService: ComplaintService) {
     }
@@ -47,8 +52,46 @@ export class ComplaintComponent implements OnInit {
         this.routeSub = this.complaintService.retrieveAllComplaints().subscribe(res => {console.log(res);
             this.complaints = res;
         });
+        this.getComplaintStats();
         this.getComplaintsByType();
+        this.complaintService.getComplaintsCountByDate().subscribe(complaintsCount => {
+            const labels = [];
+            const data = [];
+            for (let i = 1; i <= 30; i++) {
+                labels.push(i);
+                data.push(complaintsCount[i - 1]);
+            }
+            const ctx = this.complaintsChartRef.nativeElement.getContext('2d');
+            this.complaintsChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Complaints',
+                        data: data,
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        });
     }
+
+    ngOnDestroy(): void {
+        if (this.complaintsChart) {
+            this.complaintsChart.destroy();
+        }
+    }
+
+
 
     deleteComplaint(id: string) {this.complaintService.deleteComplaint(id).subscribe(p => {
         console.log('delete');
@@ -62,12 +105,87 @@ export class ComplaintComponent implements OnInit {
                     this.router.navigate(['/superadmin/complaint']);
                 });
             }
-    getComplaintsByType() {
-        this.complaintService
-            .getComplaintsByType(this.selectedType)
-            .subscribe((res) => {
-                console.log(res);
-                this.complaintsByType = res;
-            });
+
+    getComplaintStats() {
+        this.complaintService.getComplaintStats().subscribe(
+            data => {
+                this.generateChart(data);
+            },
+            error => {
+                console.log(error);
+            }
+        );
     }
+
+    getComplaintsByType() {
+        this.complaintService.getComplaintsByType().subscribe(
+            data => {
+                this.generateChartt(data);
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+
+    generateChart(stats: any) {
+        const chartData = {
+            labels: Object.keys(stats),
+            datasets: [{
+                data: Object.values(stats),
+                backgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56',
+                ],
+                hoverBackgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56',
+                ]
+            }]
+        };
+
+        const canvas = this.pieChart.nativeElement;
+        this.chart = new Chart(canvas, {
+            type: 'pie',
+            data: chartData,
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        });
+    }
+
+
+    generateChartt(stats: any) {
+        const chartData = {
+            labels: Object.keys(stats),
+            datasets: [{
+                data: Object.values(stats),
+                backgroundColor: [
+                    '#ff6392',
+                    '#63dbff',
+                    '#FFCE56',
+                ],
+                hoverBackgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56',
+                ]
+            }]
+        };
+
+        const canvas = this.pieChartt.nativeElement;
+        this.chart = new Chart(canvas, {
+            type: 'pie',
+            data: chartData,
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        });
+    }
+
+
 }
